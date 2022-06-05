9d80e072f7bce035a2bede93b6be54e06a8d1fd7
RabbitBinary <86431557+RabbitBinary@users.noreply.github.com>
2022-06-05 12:21:15 +0200

    commit-1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation
+} from 'react-router-dom';
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/myapps" element={<Navigate toto="/learn"/>}/>
+
+        <Route path="learn" element={<Learn/>}>
+
+          <Route path="courses" element={<Courses/>}>
+            <Route path=":courseid" element={<CourseId/>}/>
+          </Route>
+
+          <Route path="bundles" element={<Bundles/>}/>
+
+        </Route>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </Router>
+
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Home() {
+  return (
+    <div>
+      <h1>Home Route</h1>
+      <NavLink to={`/learn`}>Learn</NavLink>
+    </div>
+  )
+}
+
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
+      <Link to="/learn/courses">Courses</Link> |
+      <Link to="/learn/bundles">Bundles</Link>
+      <Outlet/>
+    </div>
+  )
+}
+
+function Courses() {
+  const courseList = ["React", "Angular", "Vue", "Nodejs"]
+  const randomCourseName = courseList[Math.floor(Math.random() * courseList.length)]
+  return (
+    <div>
+      <p>Courses list</p>
+      <p>Courses card</p>
+
+      <p>Courses list</p>
+      <NavLink
+        style={({isActive}) => {
+          return {
+            backgroundColor: isActive ? "pink" : "yellow"
+          };
+        }}
+        to={`/learn/courses/${randomCourseName}`}>
+        {randomCourseName}
+      </NavLink> (Refresh page/Click)|
+      <NavLink to={`/learn/courses/tests`}>tests</NavLink>
+
+      <Outlet/>
+    </div>
+  )
+}
+
+function Bundles() {
+  return (
+    <div>
+      <p>Bundle list</p>
+      <p>Bundle card</p>
+    </div>
+  )
+}
+
+function CourseId() {
+  const navigate = useNavigate()
+  const {courseid} = useParams();
+  return (
+    <div>
+      <p>URL Params is: {courseid}</p>
+      <button
+      onClick={() => {
+        navigate("/dashboard", {state:"399"});
+      }}
+      >Price</button>
+    </div>
+  )
+}
+
+function Dashboard() {
+  const location = useLocation()
+  return (
+    <div>
+      <p>Info is here: {location.state}</p>
+
+    </div>
+  )
+}
